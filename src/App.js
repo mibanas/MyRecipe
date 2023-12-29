7ebebffb9302b6c7148f13e19fd2bf040ba855e5
mibanas <sanabi_mohamed@hotmail.fr>
2023-12-29 10:22:55 +0100

    initiat the project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router';
 import './App.css';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Home from './Components/Myhome/Home';
+import AllRecepies from './Components/AllRecepies/AllRecepies';
+import RecepyDetail from './Components/RecepyDetail/RecepyDetail';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/AllRecepies' element={<AllRecepies />} />
+          <Route path='/RecepyDetail' element={<RecepyDetail />} />
+        </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/RecepyDetail/RecepyDetail.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RecepyDetail = () => {
+  return (
+    <div>RecepyDetail</div>
+  )
+}
+
+export default RecepyDetail
