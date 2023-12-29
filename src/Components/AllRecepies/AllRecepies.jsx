bc338b6a7f80fba1636284aedef4bca60df79b52
mibanas <sanabi_mohamed@hotmail.fr>
2023-12-29 11:36:14 +0100

    jsonserver

--- src/Components/AllRecepies/AllRecepies.jsx
+++ src/Components/AllRecepies/AllRecepies.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import '../../Assets/styles/allRecepies.css';
+
+// ============= assets ============= 
+import platimage from '../../Assets/img/platimage.jpeg'
 
 const AllRecepies = () => {
   return (
-    <div>AllRecepies</div>
+    <div className='AllRecepies'>
+        <div className="AllRecepies-image">
+            {/* <img src={platimage} alt="" /> */}
+        </div>
+        <div className="AllRecepies-filres"></div>
+        <div className="AllRecepies-products"></div>
+    </div>
   )
 }
 
