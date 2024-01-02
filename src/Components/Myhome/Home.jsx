c319459ac5dda75760000a5bb9a82e39db497e9c
mibanas <sanabi_mohamed@hotmail.fr>
2024-01-02 12:46:11 +0100

    fix header

--- src/Components/Myhome/Home.jsx
+++ src/Components/Myhome/Home.jsx
@@ -1,114 +1,42 @@
 import React from 'react'
-import image from "../../Assets/img/circle.png"
-import image0 from "../../Assets/img/Girlcircle.png"
+import '../../Assets/styles/home.css';
 
 
-import picture from "../../Assets/img/Intersect.png"
-import { Link } from 'react-router-dom'
-import "../../Assets/styles/home.css"
-import image1 from "../../Assets/img/pngwing 4.png"
-import image2 from "../../Assets/img/pngwing 6.png"
-import image3 from "../../Assets/img/pngwing 3.png"
-import image4 from "../../Assets/img/pngwing 7.png"
+// ===================== image ===================== 
+import Girlcircle from '../../Assets/img/Girlcircle.png';
 
 
 
 
-import img_10 from "../../Assets/img/img_10.jpg"
-
-
-const list = [
-  "learn new recipes ",
-  "Experiment with food ",
-  "Write your own recipes ",
-  "Know nutrition facts ",
-  "Get ranked ",
-  "Get cooking tips ",
-
-]
 
 function Home() {
   return (
 
+    <div className="Principale">
+    <div className="Principale-Contents">
 
-    <div className="welcome-home">
+      <div className="Principale-Content0">
+        Dive into Delights Of Delectable
+        <span className="Principale-food"> Food</span>
+      </div>
 
-      <div className="home">
-        <div className="header">
-          <img src={image0} alt="" />
-        </div>
-        <div className="home-text">
-          <h1 className="primary-heading">
-            What Are We About?
-          </h1>
-          <p className="primary-text">FOodie is a place  where u can please ur soul  <br />
-            and toumy  with delicious food recipes of all cuisine. <br />
-            and our service is absolutly  free so start exploring now. <br />  </p>
-          <Link to="/menu">
-            <button className="btn">TRY NOW</button>
-          </Link>
-        </div>
-        {/* <div className="home-image-section">
-          <img src={picture} alt="" className="center" />
-        </div> */}
-        <div className="section improve-skills flex items-center justify-center min-h-screen">
-          <div className="col img">
-            <img src={img_10} alt="" />
-          </div>
-          <div className="col typography">
-            <h1 className="title "  >Improve Your Culinary Skills 😉</h1>
-            {list.map((item, index) => (<p className="skill-item" key={index} >{item} </p>))}
+      <div className="Principale-Content1">
+        Where Each Plate Weaves a Story of Culinary Mastery and Passionate Craftsmanship
+      </div>
 
-            <button className="btn">DISCOVER NOW</button>
-          </div>
-
-
-        </div>
-
+      <div className="principale-order">
+        <span className='principale-order-title'>
+          Order Now
+        </span>
       </div>
       
-      <div className='categorie'>
-        <h1 className='titlee'>Our Popular Categorie :</h1>
-        <div className='categorie-list'>
-          <span>
-          <img src={image1} alt="" />
-          <h2>Main Dish</h2>
-          </span>
-
-          <span>
-          <img src={image2} alt="" />
-          <h2>Break fast</h2>
-          </span>
-          <span>
-          <img src={image3} alt="" />
-          <h2>Dessert</h2>
-
-          </span>
-          <span>
-          <img src={image4} alt="" />
-          <h2>Browse All</h2> 
-
-          </span>
-         
-
-          
-          {/*  */}
-
-          
-          {/* */}
-
-          
-          {/**/}
-
-
-        </div>
-      </div>
     </div>
 
+    <div className="Principale-imgs">
+      <img className='Principale-img' src={Girlcircle} alt="girl eat" />
+    </div>
 
-
-
-
+  </div>
 
 
   )
