825e4f8683853ac6bd060f840086dcae46bf054c
imaneouarid <imaneomg@gmail.com>
2024-01-03 17:01:50 +0100

    home page and cantact us

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,19 +1,20 @@
-import React from "react";
+import React  from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import img10 from "../Assets/img/pex.jpg";
 import "../Assets/styles/Contact.css"; 
 
-
 export default function Contact() {
+  const notify = () => toast("message sent succesfully!");
+  
   return (
     <div className="contact">
-      <div className="leftSide" style={{ backgroundImage: `url(${img10})` }}
-
-      >
+      <div className="leftSide" style={{ backgroundImage: `url(${img10})` }} >
 
       </div>
       <div className="rightSide">
         <h1>Contact Us</h1>
-        <form id="contact-form" method="POST">
+        <form id="contact-form" >
           <label htmlFor="name">Full Name</label>
           <input name="name" type="text" placeholder="Enter full name" />
           <label htmlFor="email">Email</label>
@@ -25,9 +26,50 @@ export default function Contact() {
             placeholder="Enter message"
             required
           ></textarea>
-          <button type="Submit">Send Message</button>
+          <button type="Submit" onClick={notify}>Send Message</button>
+          <ToastContainer />
+
         </form>
       </div>
     </div>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+// const [plats, setPlats] = useState([]);
+  // const [troisPlats, setTroisPlats] = useState([]);
+
+  // useEffect(() => {
+  //   const getAllPlats = async () => {
+  //     try {
+  //       const responsePlats = await fetch('http://localhost:3000/plats');
+  //       if (!responsePlats.ok) {
+  //         return 'errors';
+  //       }
+
+  //       const dataPlats = await responsePlats.json();
+  //       setPlats(dataPlats);
+  //       console.log(dataPlats);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   };
+
+  //   getAllPlats();
+  // }, []);
+
+  // useEffect(() => {
+  //   if (plats.length >= 3) {
+  //     const selectedTroisPlats = plats.slice(0, 3);
+  //     setTroisPlats(selectedTroisPlats);
+  //   }
+  // }, [plats]);
--- src/Components/Myhome/Home.jsx
+++ src/Components/Myhome/Home.jsx
@@ -1,5 +1,9 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../../Assets/styles/home.css';
+import { Link } from 'react-router-dom';
+import axios from "axios";
+
+// import blogLis from
 
 
 // ===================== image ===================== 
@@ -10,35 +14,94 @@ import Girlcircle from '../../Assets/img/Girlcircle.png';
 
 
 function Home() {
+
+  const [plats, setPlats] = useState([]);
+  const [troisPlats, setTroisPlats] = useState([]);
+
+  useEffect(() => {
+    const getAllPlats = async () => {
+      try {
+        const responsePlats = await fetch('http://localhost:3000/plats');
+        if (!responsePlats.ok) {
+          return 'errors';
+        }
+
+        const dataPlats = await responsePlats.json();
+        setPlats(dataPlats);
+        console.log(dataPlats);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getAllPlats();
+  }, []);
+
+  useEffect(() => {
+    if (plats.length >= 3) {
+      const selectedTroisPlats = plats.slice(0, 3);
+      setTroisPlats(selectedTroisPlats);
+    }
+  }, [plats]);
+
+
   return (
 
-    <div className="Principale">
-    <div className="Principale-Contents">
+    <>
+      <div className="Principale">
+        <div className="Principale-Contents">
 
-      <div className="Principale-Content0">
-        Dive into Delights Of Delectable
-        <span className="Principale-food"> Food</span>
+          <div className="Principale-Content0">
+            Dive into Delights Of Delectable
+            <span className="Principale-food"> Food</span>
+          </div>
+
+          <div className="Principale-Content1">
+            Where Each Plate Weaves a Story of Culinary Mastery and Passionate Craftsmanship
+          </div>
+
+          <div className="principale-order">
+            <Link to="/menu" className="principale-order-title">Discover now</Link>
+
+
+          </div>
+
+        </div>
+
+        <div className="Principale-imgs">
+          <img className='Principale-img' src={Girlcircle} alt="girl eat" />
+        </div>
+
+
       </div>
+      <div className='AllPRoducts'>
+        <div className="AllPRoducts-title">Customer Favorites</div>
+        <div className="AllPRoducts-title1">Popular Catagories</div>
+        <div className='AllPRoducts-card'>
 
-      <div className="Principale-Content1">
-        Where Each Plate Weaves a Story of Culinary Mastery and Passionate Craftsmanship
+        {
+          troisPlats.map((plats, i) =>
+
+              <div className="AllPRoduc-card">
+
+                <div key={i}>
+                  <div className="AllPRoduct-card-image">
+                    <img src={plats.image} alt="" className='AllPRoduct-card-img' />
+                  </div>
+                  <h2 className='AllPRoduct-card-recepies'>{plats.nom} </h2>
+                  <h2>{plats.popularite} </h2>
+                </div>
+
+              </div>
+
+
+          )
+
+        }
+                    </div>
+
       </div>
-
-      <div className="principale-order">
-        <span className='principale-order-title'>
-          Order Now
-        </span>
-      </div>
-      
-    </div>
-
-    <div className="Principale-imgs">
-      <img className='Principale-img' src={Girlcircle} alt="girl eat" />
-    </div>
-
-  </div>
-
-
+    </>
   )
 }
 
@@ -50,32 +113,43 @@ export default Home
 
 
 
-{/* <div className="home-bannerImage-container">
-                    <img src={BannerBackground} alt="" />
-                </div>
-                <div className="home-text-section">
-                    <h1 className="primary-heading">
-                        What Are We About?
-                    </h1>
-                    <p className="primary-text">FOodie is a place  where u can please ur soul  <br />
-                    and toumy  with delicious food recipes of all cuisine. <br />
-                            and our service is absolutly  free so start exploring now. <br />  </p>
-                    <button className="btn">EXPLORE NOW</button>
-                </div>
-                <div className="home-image-section">
-              <img src={pic} alt=""  className="center"/>
-            </div>
-            <div className="section improve-skills flex items-center justify-center min-h-screen">
-      <div className="col img">
-        <img src={img_10}  alt="" />
+
+
+{/* <div className='AllPRoducts'>
+      <div className="AllPRoducts-title">Customer Favorites</div>
+      <div className="AllPRoducts-title1">Popular Catagories</div>
+      <div className="AllPRoducts-card">
+
+        <div className="AllPRoduc-card">
+          <div className="AllPRoduct-card-image">  
+            <img className='AllPRoduct-card-img' src={sandwish} alt="" />
+          </div>
+          <span className='AllPRoduct-card-recepies'>Main Dish</span>
+          <span className='AllPRoduct-card-nbrecepies'>(86 dishes)</span>
+        </div>
+
+        <div className="AllPRoduc-card">
+          <div className="AllPRoduct-card-image">  
+            <img className='AllPRoduct-card-img' src={sandwish} alt="" />
+          </div>
+          <span className='AllPRoduct-card-recepies'>Main Dish</span>
+          <span className='AllPRoduct-card-nbrecepies'>(86 dishes)</span>
+        </div>
+
+        <div className="AllPRoduc-card">
+          <div className="AllPRoduct-card-image">  
+            <img className='AllPRoduct-card-img' src={sandwish} alt="" />
+          </div>
+          <span className='AllPRoduct-card-recepies'>Main Dish</span>
+          <span className='AllPRoduct-card-nbrecepies'>(86 dishes)</span>
+        </div>
+
+        <div className="AllPRoduc-card">
+          <div className="AllPRoduct-card-image">  
+            <img className='AllPRoduct-card-img' src={sandwish} alt="" />
+          </div>
+          <span className='AllPRoduct-card-recepies'>Main Dish</span>
+          <span className='AllPRoduct-card-nbrecepies'>(86 dishes)</span>
+        </div>
       </div>
-      <div className="col typography">
-        <h1 className="title "  >Improve Your Culinary Skills 😉</h1>
-        { list.map((item,index)=>( <p className="skill-item" key={index} >{item} </p> ))}
-
-        <button className="btn">DISCOVER NOW</button>
-      </div>
-
-
-    </div>
-     */}
+    </div> */}
